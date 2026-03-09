6e8eb60ea98e5208013e629b90b257b95e3c6ee0
Lebron <alebron1290@gmail.com>
2026-03-08 23:40:05 -0400

    FixedWords

--- routes/words.js
+++ routes/words.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {readFile, writeFile} = require('fs').promises;
+const {readFile, writeFile} = require('fs').promises; //Destructuring
 
 router.get('/', (req, res)=>{
     res.send('Word Home Page');
 });
 
-router.get('/wotd', (req, res)=>{
-    res.render('wotd');
-})
+router.get('/wotd', async (req, res) =>{
+    let wordArray = await getWordFromDictionary();
+    let [word, part, definition] = wordArray;
+    res.render('wotd',{word:word, part:part, definition:definition});
+    //Do something with that function up here
+});
 
+let getWordFromDictionary = async ()=>{
+    try{
+        const data = await readFile('resources/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+        let randomNumber = parseInt(Math.random()*lines.length);
+        let randomLine = lines[randomNumber];
+        let wordArray = randomLine.split('\t');
+        console.log(wordArray);
+        return wordArray;
+    } catch(err){
+        console.log("There was an error reading the file:", err);
+    }
+};
 module.exports = router;
